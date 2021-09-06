d1013790a13a13dc18ea1951f9174c2a08d369b6
Nhựt Phạm <66513124+PhamMinhNhut-CDTH18PMC@users.noreply.github.com>
2021-09-06 12:11:03 +0700

    continue fix config

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 products = Product.create([
-    {name:"Áo thun", price: 100000, image_product: "image_1.jpg", active:1},
-    {name:"Áo nam", price: 100000, image_product: "image_2.jpg", active:1},
-    {name:"Áo khoác", price: 100000, image_product: "image_3.jpg", active:1},
-    {name:"Áo khoác đen", price: 100000, image_product: "image_4.jpg", active:1},]);
+{name:"Áo thun", price: 100000, image_product: "image_1.jpg", active:1},
+{name:"Áo nam", price: 100000, image_product: "image_2.jpg", active:1},
+{name:"Áo khoác", price: 100000, image_product: "image_3.jpg", active:1},
+{name:"Áo khoác đen", price: 100000, image_product: "image_4.jpg", active:1},]);
