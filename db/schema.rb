53a57c69d9768e72d6b0e9c49a2d1a37721fff79
DESKTOP-QJQR638\Minh Nhut <nhatminh785@gmail.com>
2021-11-20 17:48:51 +0700

    test ProductController

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_141657) do
+ActiveRecord::Schema.define(version: 2021_10_21_114455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2021_09_28_141657) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "addresses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "street"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -75,4 +83,5 @@ ActiveRecord::Schema.define(version: 2021_09_28_141657) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "users"
 end
---
+++ spec/factories/product_factory.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory  :product do
+        name  {"Áo đỏ"}
+        price {1000000}
+        category_id {1}
+        active {true}
+    end
+end
