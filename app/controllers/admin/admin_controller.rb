c30b40b26a2088b70a95748b1d0aee6d4e078a67
robinnguyen31 <73183999+robinnguyen31@users.noreply.github.com>
2022-03-01 16:35:00 +0700

    Update admin_controller.rb

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,12 +1,14 @@
 class Admin::AdminController < ActionController::Base
-    helper_method :current_admin
-    helper_method :logged_in?
+  layout 'admin'
+  
+  helper_method :current_admin
+  helper_method :logged_in?
 
-    layout 'admin'
-    def current_admin
-        Admin::Admin.find_by(id: session[:admin_id])
-    end
-    def logged_in?
-        !current_admin.nil?
-    end
+  def current_admin
+      Admin::Admin.find_by(id: session[:admin_id])
+  end
+  
+  def logged_in?
+      !current_admin.nil?
+  end
 end
